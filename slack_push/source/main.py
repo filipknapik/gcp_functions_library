b658377051b67f2bd1c5bcdfae3b4ee2688a25f0
Filip Knapik <knapik@knapik-macbookpro.roam.corp.google.com>
2019-06-05 20:51:25 +0200

    slack fixes

--- slack_push/source/main.py
+++ slack_push/source/main.py
@@ -1,9 +1,20 @@
-import random, json
-from flask import jsonify
+import requests
+import json 
 
 def execute(request):
-    randomNum = random.randint(1,10)
-    output = {"slackresponse":randomNum}
-    return jsonify(output)
+    request_json = request.get_json()
 
+    url_param = getParam('slackurl', request_json)
+    message = getParam('message', request_json)
+
+    body = {'text': message} 
+    headers_struct = {'Content-type': 'application/json'}
    
+    r = requests.post(url = url_param, data = json.dumps(body), headers = headers_struct) 
+    return r.text
+
+def getParam(param, request_json):
+    if request_json and param in request_json:
+        return request_json[param]
+    else:
+        return ''
