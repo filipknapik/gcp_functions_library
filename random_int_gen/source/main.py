201493e54e6eff80f1041068106caf3166db56ae
Filip Knapik <knapik@knapik-macbookpro.roam.corp.google.com>
2019-06-05 10:15:45 +0200

    randomadded

--- random_int_gen/source/main.py
+++ random_int_gen/source/main.py
@@ -1,1 +1,9 @@
-hello world
+import random, json
+from flask import jsonify
+
+def rand_gen(request):
+    randomNum = random.randint(1,10)
+    output = {"randomnum":randomNum}
+    return jsonify(output)
+
+   
