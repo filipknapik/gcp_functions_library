7175ddf3d2a19c1d81f3e868b693c09da26e0dfe
Filip Knapik <knapik@knapik-macbookpro.roam.corp.google.com>
2019-06-05 10:30:03 +0200

    changed to execute

--- random_int_gen/source/main.py
+++ random_int_gen/source/main.py
@@ -1,7 +1,7 @@
 import random, json
 from flask import jsonify
 
-def rand_gen(request):
+def execute(request):
     randomNum = random.randint(1,10)
     output = {"randomnum":randomNum}
     return jsonify(output)
