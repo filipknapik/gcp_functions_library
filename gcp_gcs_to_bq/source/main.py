300084be767835fb2f4e9cb250eeae5e53fbc8c0
Filip Knapik <knapik@knapik-macbookpro.roam.corp.google.com>
2019-06-09 17:08:33 +0200

    added gcs to bq

---
+++ gcp_gcs_to_bq/source/main.py
@@ -0,0 +1,24 @@
+from google.cloud import bigquery
+from flask import jsonify
+
+def execute(request):
+    request_json = request.get_json()
+    client = bigquery.Client()
+    
+    dataset = request_json['dataset']
+    source = request_json['source']
+    table = request_json['table']
+    
+    dataset_ref = client.dataset(dataset)
+    
+    job_config = bigquery.LoadJobConfig()
+    job_config.autodetect = True
+    job_config.source_format = bigquery.SourceFormat.CSV
+
+    load_job = client.load_table_from_uri(
+        source, dataset_ref.table(table), job_config=job_config
+    )  
+    print("Starting job {}".format(load_job.job_id))
+    load_job.result()  # Waits for table load to complete.
+    print("Job finished.")
+    return jsonify({"Result":"OK"})
